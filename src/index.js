714bf2dde4040190492f57c4cf64b090bbca4584
Elizinha123 <elizaxavier2@gmail.com>
2022-08-24 09:01:33 -0300

    linha magica

--- src/index.js
+++ src/index.js
@@ -7,14 +7,12 @@ import App from './pages/App/App';
 import Tarefas from './pages/Tarefas';
 import Renderizacao from './pages/Renderizacao';
 import SituacaoAluno from './pages/SituacaoAluno';
+import LinhaMagica from './pages/linhaMagica';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SituacaoAluno/>
-    <Renderizacao/>
-    <Tarefas/>
-    <App/>
+    <LinhaMagica/>
    
   </React.StrictMode>
 );
---
+++ src/pages/linhaMagica/index.js
@@ -0,0 +1,19 @@
+import './index.scss'
+
+export default function LinhaMagica() {
+
+
+    return(
+        <section className='magic'>
+            <nav >
+               
+
+                
+                    <a href='#INICIO'> INICIO </a>
+                    <a href='# EM CARTAZ'> EM CARTAZ </a>
+                    <a href='#EM BREVE'> EM BREVE </a>
+                    <a href='#SNACK BAR'> SNACK BAR </a>
+            </nav>
+        </section>
+    )
+}
