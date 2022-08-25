b03c4b7c297bcd06e713c53de62c6277ac9ef529
manuellashinee <manujesus.vasconcelos@gmail.com>
2022-08-25 11:48:36 -0300

    nao deu certo o segundo carrossel AINDA

--- src/pages/linhaMagica/index.js
+++ src/pages/linhaMagica/index.js
@@ -1,7 +1,22 @@
+import { tns } from 'tiny-slider'
 import './index.scss'
 
 export default function LinhaMagica() {
-
+    let slider = tns({
+        container : ".my-slider",
+        "slideBy": 1,
+        "speed":400,
+        "nav": false,
+        controlsContainer:".controls",
+        prevButton : ".previous",
+        nextButton : ".next",
+        responsive : {
+            1240 : {
+                items : 3,
+                gutter: 20
+            }
+        }
+    })
 
     return(
         <section id ="slider">
@@ -16,15 +31,27 @@ export default function LinhaMagica() {
                                 </h1>
                             </div>
                             <div id='controls'>
-                                <button className='previous'><i class="fa-solid fa-angles-left"></i></button>
-                                <button className='next'><i class="fa-solid fa-angles-right"></i></button>
+                                <button className='previous'><i className='fa-solid fa-angles-left'></i></button>
+                                <button className='next'><i className='fa-solid fa-angles-right'></i></button>
 
                             </div>
                         </div>
                         <div className='my-slider'>
-                            <div className='slide'>
+                         <div>
+                           <div className='slide'>
                                <div className='slide-img img-1'></div> 
-                            </div>    
+                            </div>
+                         </div>   
+                         <div>
+                           <div className='slide'>
+                               <div className='slide-img img-2'></div> 
+                            </div>
+                         </div>   
+                         <div>
+                           <div className='slide'>
+                               <div className='slide-img img-3'></div> 
+                            </div>
+                         </div>    
                          </div>
                     </div>
                 </div>
