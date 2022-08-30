be588965d42f85161abaf7463828a32bdca8f459
manuellashinee <manujesus.vasconcelos@gmail.com>
2022-08-30 09:44:36 -0300

    defeituoso

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ import Tarefas from './pages/Tarefas';
 import Renderizacao from './pages/Renderizacao';
 import SituacaoAluno from './pages/SituacaoAluno';
 import LinhaMagica from './pages/multi1';
+import Multi2 from './pages/multi2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LinhaMagica/>
+    <Multi2/>
    
   </React.StrictMode>
 );
--- src/pages/multi2/index.js
+++ src/pages/multi2/index.js
@@ -2,6 +2,7 @@ import './index.scss'
 
 export default function MultiSlider() {
     return(
+        <section className='bodi'>
         <div className='slide-container'>
             <span className='slider-span' id='slider-span1'></span>
             <span className='slider-span' id='slider-span2'></span>
@@ -13,35 +14,37 @@ export default function MultiSlider() {
 
             <div className='image-slider'>
                 <div className='slides-div' id='slide-1'>
-                    <img src="../images/buzz.jpg" id='img1'/>
+                    <img clasName='odi' src="../images/buzz.jpg" id='img1'/>
                     <a href='#slider-span1' className='button' id='button-1'></a>
                 </div>
                 <div className='slides-div' id='slide-2'>
-                    <img src="../images/" id='img2'/>
+                    <img clasName='odi' src="../images/mundo.jpg" id='img2'/>
                     <a href='#slider-span1' className='button' id='button-2'></a>
                 </div>
                 <div className='slides-div' id='slide-3'>
-                    <img src="../images/buzz.jpg" id='img3'/>
+                    <img clasName='odi' src="../images/red.jpg" id='img3'/>
                     <a href='#slider-span1' className='button' id='button-3'></a>
                 </div>
                 <div className='slides-div' id='slide-4'>
-                    <img src="../images/buzz.jpg" id='img4'/>
+                    <img clasName='odi' src="../images/raya.webp" id='img4'/>
                     <a href='#slider-span1' className='button' id='button-4'></a>
                 </div>
                 <div className='slides-div' id='slide-5'>
-                    <img src="../images/buzz.jpg" id='img5'/>
+                    <img clasName='odi' src="../images/minions.jpg" id='img5'/>
                     <a href='#slider-span1' className='button' id='button-5'></a>
                 </div>
                 <div className='slides-div' id='slide-6'>
-                    <img src="../images/buzz.jpg" id='img6'/>
+                    <img clasName='odi'src="../images/s.webp" id='img6'/>
                     <a href='#slider-span1' className='button' id='button-6'></a>
                 </div>
                 <div className='slides-div' id='slide-7'>
-                    <img src="../images/buzz.jpg" id='img7'/>
+                    <img clasName='odi' src="../images/luca.webp" id='img7'/>
                     <a href='#slider-span1' className='button' id='button-7'></a>
                 </div>
             </div>
 
         </div>
+
+        </section>
     )
 }
